45c0ede5e0cc88cf7c381af7cfc9732883283305
nOxx <smc.8645@gmail.com>
2020-02-28 14:23:56 -0500

    Elements centered and changed their style

--- script.js
+++ script.js
@@ -8,19 +8,18 @@ let colorDiv = document.getElementById("colorDiv");
 buttonDiv.appendChild(colorDiv);
 
 const black = document.createElement("button");
-const red = document.createElement("button");
+const green = document.createElement("button");
 const blue = document.createElement("button");
 const yellow = document.createElement("button");
+const rainbow = document.createElement("button");
 colorDiv.appendChild(black);
-colorDiv.appendChild(red);
+colorDiv.appendChild(green);
 colorDiv.appendChild(blue);
 colorDiv.appendChild(yellow);
+colorDiv.appendChild(rainbow);
 
 const clear = document.createElement("button");
 buttonDiv.appendChild(clear);
-
-const rainbow = document.createElement("button");
-buttonDiv.appendChild(rainbow);
 
 toy.appendChild(grid);
 toy.appendChild(buttonDiv);
@@ -29,16 +28,15 @@ toy.appendChild(buttonDiv);
 
 
 //----- GRID ------------------>
-
 function createGrid() {
-    //resetGrid();
+    resetGrid();
     grid.style.gridTemplateColumns = `repeat(${10}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${10}, 1fr)`;
 
     for(i = 0; i < 100; i++) { 
         let div = document.createElement("div");
         div.style.backgroundColor = "white";
-        div.style.border = "1px solid black";
+        div.style.border = "1px #FFFFFF";
         grid.appendChild(div);
 
     }
@@ -55,26 +53,65 @@ function resetGrid() {
 //------------------------------>
 
 //----- BUTTONS STYLE----------------->
+buttonDiv.style.padding = "5%";
+
+colorDiv.style.display = "flex";
+colorDiv.style.justifyContent = "center";
+colorDiv.style.paddingBottom = "20px";
 
 black.textContent = "BLACK";
 black.style.backgroundColor = "black";
 black.style.color = "white";
+black.style.borderRadius = "50px";
+black.style.fontSize = "1rem";
+black.style.width = "6rem";
+black.style.cursor = "pointer";
+black.style.margin = "0 10px";
 
-red.textContent = "RED";
-red.style.backgroundColor = "red";
-red.style.color = "white";
+green.textContent = "GREEN";
+green.style.backgroundColor = "green";
+green.style.color = "white";
+green.style.borderRadius = "50px";
+green.style.fontSize = "1rem";
+green.style.width = "6rem";
+green.style.cursor = "pointer";
+green.style.margin = "0 10px";
 
 blue.textContent = "BLUE";
 blue.style.backgroundColor = "blue";
 blue.style.color = "white";
+blue.style.borderRadius = "50px";
+blue.style.fontSize = "1rem";
+blue.style.width = "6rem";
+blue.style.cursor = "pointer";
+blue.style.margin = "0 10px";
 
 yellow.textContent = "YELLOW";
 yellow.style.backgroundColor = "yellow";
 yellow.style.color = "white";
-
-clear.textContent = "CLEAR";
+yellow.style.borderRadius = "50px";
+yellow.style.fontSize = "1rem";
+yellow.style.width = "6rem";
+yellow.style.cursor = "pointer";
+yellow.style.margin = "0 10px";
 
 rainbow.textContent = "RAINBOW";
+rainbow.style.backgroundColor = "red";
+rainbow.style.color = "white";
+rainbow.style.borderRadius = "50px";
+rainbow.style.fontSize = "1rem";
+rainbow.style.width = "6rem";
+rainbow.style.cursor = "pointer";
+rainbow.style.margin = "0 10px";
+
+clear.textContent = "CLEAR";
+clear.style.display = "block";
+clear.style.height = "5rem";
+clear.style.width = "5rem";
+clear.style.fontSize = "1rem";
+clear.style.borderRadius = "60%";
+clear.style.cursor = "pointer";
+clear.style.margin = "auto";
 
 //------------------------------>
 
